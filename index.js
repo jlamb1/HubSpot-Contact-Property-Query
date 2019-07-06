fcc7c4fc0ed2f90f13130a83519c4d677b0589b2
test <test123234567890@test.com>
2019-07-06 14:26:21 -0400

    standardjs

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-require('dotenv').config();
-const http = require('http');
-const prompt = require('prompt');
+require('dotenv').config()
+const http = require('http')
+const prompt = require('prompt')
 
-prompt.start();
+prompt.start()
 
 prompt.get(['email', 'value'], (err, result) => {
-  const url = `http://api.hubapi.com/contacts/v1/contact/email/${result.email}/profile?hapikey=${process.env.HAPI_KEY}`;
+  if (err) {
+    console.log(err)
+  }
+  const url = `http://api.hubapi.com/contacts/v1/contact/email/${result.email}/profile?hapikey=${process.env.HAPI_KEY}`
   http.get(url, (res) => {
-    let body = '';
+    let body = ''
     res.on('data', (chunk) => {
-      body += chunk;
-    });
+      body += chunk
+    })
     res.on('end', () => {
-      const searchedProperty = JSON.parse(body).properties[result.value].value;
-      console.log(searchedProperty);
-    });
-  }).on('error', (e) => { console.log(`Got an error: ${e}`); });
-});
+      const searchedProperty = JSON.parse(body).properties[result.value].value
+      console.log(searchedProperty)
+    })
+  }).on('error', (e) => { console.log(`Got an error: ${e}`) })
+})
